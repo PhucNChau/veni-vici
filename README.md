7ce7e3440ce98fec9f5488eb7b75e3d997aa566a
Phuc Chau <phucnchau@gmail.com>
2024-10-10 15:33:33 -0400

    Add left and right side bar. Add logic to add, remove and filter attributes. Add gif video.

--- README.md
+++ README.md
@@ -1,31 +1,32 @@
-# Web Development Project 4 - *Name of App Here*
+# Web Development Project 4 - *MeowMatch*
 
 Submitted by: **Phuc Chau**
 
-This web app: **insert description**
+This web app: **A place for discovering and exploring beloved cats!**
 
-Time spent: **X** hours spent in total
+Time spent: **11** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-- [ ] **Clicking a button creates a new API fetch request and displays at least three attributes from the returned JSON data**
-- [ ] **Only one item/API call is viewable at a time**
-- [ ] **API calls appear random to the user**
-- [ ] **At least one image is displayed per API call**
-- [ ] **Clicking on a displayed value for one attribute adds it to a displayed ban list**
-  - [ ] To ensure an accurate grade, your recording **must** show that when clicked, an attribute in the ban list is immediately removed from the list of banned attributes
-- [ ] **Attributes on the ban list prevent further images/API results with that attribute from being displayed**
+- [x] **Clicking a button creates a new API fetch request and displays at least three attributes from the returned JSON data**
+- [x] **Only one item/API call is viewable at a time**
+- [x] **API calls appear random to the user**
+- [x] **At least one image is displayed per API call**
+- [x] **Clicking on a displayed value for one attribute adds it to a displayed ban list**
+  - [x] To ensure an accurate grade, your recording **must** show that when clicked, an attribute in the ban list is immediately removed from the list of banned attributes
+- [x] **Attributes on the ban list prevent further images/API results with that attribute from being displayed**
 
 The following **optional** features are implemented:
 
-- [ ] Multiple types of attributes can be added to the ban list
-- [ ] Users can see a stored history of their previously viewed items from their session
+- [x] Multiple types of attributes can be added to the ban list
+- [x] Users can see a stored history of their previously viewed items from their session
 
 The following **additional** features are implemented:
 
-* [ ] List anything else that you added to improve the site's functionality!
+* [x] List anything else that you added to improve the site's functionality!
+* [x] Prevent add the same attribute multiple time.
 
 ## Video Walkthrough
 
--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,14 @@ const App = () => {
     breed_name: "",
     breed_origin: "",
     breed_life_span: "",
-  })
-
-  var listOfCurrentImages = null;
+  });
+  const [banList, setBanList] = useState({
+    breed_weight: new Set(),
+    breed_name: new Set(),
+    breed_origin: new Set(),
+    breed_life_span: new Set(),
+  });
+  const [imageHistory, setImageHistory] = useState([{imageUrl: "", description: ""}]);
 
   const makeQuery = () => {
     let has_breeds = 1;
@@ -24,49 +29,122 @@ const App = () => {
   const callAPI = async (query) => {
     const response = await fetch(query);
     const json = await response.json();
-    // console.log(typeof(json));
-    listOfCurrentImages = json;
-    console.log(listOfCurrentImages.pop());
-    console.log(listOfCurrentImages);
-    var index = 0;
-    if (json[index].url == null) {
-      alert("Oops! Something went wrong with that query, let's try again!")
-    }
-    else {
-      setCurrentImage(json[index].url);
-      setCatInfo({
-        breed_weight: `${json[index].breeds[0].weight.imperial} lbs`,
-        breed_name: json[index].breeds[0].name,
-        breed_origin: json[index].breeds[0].origin,
-        breed_life_span: `${json[index].breeds[0].life_span} years`,
-      });
+    var item = filterImage(json);
+    if (item == null) {
+      callAPI();
+    } else {
+      if (item.url == null) {
+        alert("Oops! Something went wrong with that query, let's try again!")
+      }
+      else {
+        setCurrentImage(item.url);
+        setCatInfo({
+          breed_weight: `${item.breeds[0].weight.imperial} lbs`,
+          breed_name: item.breeds[0].name,
+          breed_origin: item.breeds[0].origin,
+          breed_life_span: `${item.breeds[0].life_span} years`,
+        });
+        setImageHistory((prevState) => ([
+          ...prevState,
+          {
+            imageUrl: item.url,
+            description: `A ${item.breeds[0].name} cat from ${item.breeds[0].origin}`
+          }
+        ]));
+        console.log(imageHistory);
+      }
     }
   };
 
+  const filterImage = (json) => {
+    var filteredItem = null;
+    for (let item of json) {
+      if (banList.breed_life_span.has(`${item.breeds[0].life_span} years`) ||
+      banList.breed_name.has(item.breeds[0].name) ||
+      banList.breed_origin.has(item.breeds[0].origin) ||
+      banList.breed_weight.has(`${item.breeds[0].weight.imperial} years`)) {
+        continue;
+      }
+      else {
+        filteredItem = item;
+        break;
+      }
+    }
+    return filteredItem;
+  };
+
+  const addToBanList = (e) => {
+    setBanList((prevState) => ({
+      ...prevState,
+      [e.target.name]: new Set([...prevState[e.target.name], e.target.value])
+    }));
+  }
+
+  const removeFromBanList = (e) => {
+    console.log(e.target);
+    setBanList((prevState) => ({
+      ...prevState,
+      [e.target.name]: new Set([...prevState[e.target.name], prevState[e.target.name].delete(e.target.value)])
+    }));
+  }
+
   return (
     <div className='app'>
-      <h1>😻 MeowMatch 😻</h1>
-      <h3>A place for discovering and exploring beloved cats!</h3>
-      <div className='container'>
-        <div className='cat-listing'>
-          <div>
-            {catInfo && catInfo.breed_name !== "" &&
-              Object.entries(catInfo).map(([category, value], index) => (
-                <button className='attribute' type="button" key={index}>{value}</button>
-              ))
-            }
+      <div>
+        <h1>😻 MeowMatch 😻</h1>
+        <h3>A place for discovering and exploring beloved cats!</h3>
+        <div className='container'>
+          <div className='cat-listing'>
+            <div>
+              {catInfo && catInfo.breed_name !== "" &&
+                Object.entries(catInfo).map(([category, value], index) => (
+                  <button className='attribute' type="button" key={index} onClick={addToBanList} name={category} value={value}>{value}</button>
+                ))
+              }
+            </div>
+            {currentImage ? (
+              <img
+                className='cat-pic'
+                src={currentImage}
+                alt="Pic returned"
+              />
+            ) : (
+              <div></div>
+            )}
           </div>
-          {currentImage ? (
-            <img
-              className='cat-pic'
-              src={currentImage}
-              alt="Pic returned"
-            />
-          ) : (
-            <div></div>
-          )}
+          <button className='discover' type='button' onClick={makeQuery}>🔎 Discover</button>
         </div>
-        <button className='discover' type='button' onClick={makeQuery}>🔎 Discover</button>
+      </div>
+      <div className='left-sidebar'>
+        <h2>Ban List</h2>
+        <h4>Select an attribute to ban it from your search</h4>
+        <div className="attributes-container">
+          {banList && 
+            Object.entries(banList).map(([category, values], index) => (
+              values.size > 0 &&
+              <div className="attributes-list" key={index}>
+                {[...values].map((item, index) => (
+                  item &&
+                  <button className="ban-attribute" type="button" key={index} onClick={removeFromBanList} name={category} value={item}>{item}</button>
+                ))}
+              </div>
+            ))
+          }
+        </div>
+      </div>
+      <div className='right-sidebar'>
+        <h3>Our Exploration History!</h3>
+        <div className='history'>
+          {imageHistory &&
+            imageHistory.map((item, index) => (
+              item && item.imageUrl !== "" &&
+              <div key={index}>
+                <img src={item.imageUrl} alt="pic history" />
+                <p>{item.description}</p>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
